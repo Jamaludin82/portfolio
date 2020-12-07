710221ad171487b7362b95b0e21b1ec3642ee700
jamaludin82 <jjibriil25@gmail.com>
2020-12-07 12:36:54 +0100

    list item removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
                 <li><Link to= "/" className="nav-active">Home</Link></li>
                 <li><Link to= "/about">About</Link></li>
                 <li><Link to= "/contact">Contact Us</Link></li>
-                <li>hi brother</li>
+                
             </ul>
             <div className="burger">
                 <i className= "fas fa-bars fa-lg"></i>
